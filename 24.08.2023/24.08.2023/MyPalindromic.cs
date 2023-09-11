3e4ad610bdbc4403863376d5a0eaf44e88a22d6b
Rustam <rustam.niftaliev97@gmail.com>
2023-09-11 15:14:35 +0400

    DZ 24.08.2023

---
+++ 24.08.2023/24.08.2023/MyPalindromic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3
+{
+    public class MyPalindromic
+    {
+        public string text { get; set; }
+        public MyPalindromic() { }
+        public MyPalindromic(string Text) {text = Text;}
+        ~ MyPalindromic() { }
+        public string CheckPalindromic()
+        {
+            string reversedText = new string(text.Reverse().ToArray());
+
+            if (text == reversedText)
+            {
+                return $"{text} is palindromic";
+            }
+            else
+            {
+                return $"{text} is not palindromic";
+            }            
+        }
+    }
+}
---
+++ 24.08.2023/24.08.2023/kvadr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3
+{
+    class Square
+    {
+        public string Symbol { get; set; }
+        public int Length { get; set; }
+
+        public string CreateSquare()
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++) 
+                {
+                    if (i == 0 || i == Length - 1)
+                    {
+                        result.Append($"{Symbol} ");
+                        if (j == Length - 1)
+                        {
+                            result.Append("\n");
+                        }
+                    }
+                    else if(i != 0 && j == 0)
+                    {
+                        result.Append($"{Symbol} ");
+                    }
+                    else if (i != 0 && j == Length - 1)
+                    {
+                        result.Append($"{Symbol} ");
+                        result.Append("\n");
+                    }
+                    else
+                    {
+                        result.Append("  ");
+                    }
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
