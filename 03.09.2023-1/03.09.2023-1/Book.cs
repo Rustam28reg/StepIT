d58ba68bafd35d338c070b943c3952ee55b767ed
Rustam <rustam.niftaliev97@gmail.com>
2023-09-12 12:11:04 +0400

    DZ 03.09.2023

---
+++ 03.09.2023-1/03.09.2023-1/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._09._2023_1
+{
+    class Book
+    {
+        public string BookName { get; set; }
+        public string Author { get; set; }
+
+        public Book() { }
+
+        public Book(string BookName, string Author) 
+        {
+            this.BookName = BookName;
+            this.Author = Author;
+        }
+
+        public static bool operator ==(Book left, Book right)
+        {
+            return left.BookName == right.BookName;
+        }
+        public static bool operator !=(Book left, Book right)
+        {
+            return !(left.BookName == right.BookName);
+        }
+
+        public override string ToString()
+        {
+            return $"Book name - {BookName}\n" + $"Book author - {Author}";
+        }
+    }
+
+}
---
+++ 03.09.2023-1/03.09.2023-1/Shop_2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._09._2023
+{
+    class Shop
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string StoreProfile { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Area { get; set; }
+
+        public Shop() { }
+        public Shop(string name, string address, string profile, string number, string email, int area)
+        {
+            Name = name;
+            StoreProfile = profile;
+            PhoneNumber = number;
+            Email = email;
+            Address = address;
+            Area = area;
+        }
+        public static Shop operator +(Shop left, Shop right)
+        {
+            left.Area += right.Area;
+            return left;
+        }
+
+        public static Shop operator -(Shop left, Shop right)
+        {
+            left.Area -= right.Area;
+            return left;
+        }
+
+        public static bool operator <(Shop left, Shop right)
+        {
+            return left.Area < right.Area;
+        }
+        public static bool operator >(Shop left, Shop right)
+        {
+            return left.Area > right.Area;
+        }
+
+        public static bool operator !=(Shop left, Shop right)
+        {
+            return !(left.Area == right.Area);
+        }
+
+        public static bool operator ==(Shop left, Shop right)
+        {
+            return left.Area == right.Area;
+        }
+    public override string ToString()
+        {
+            return $"Shop name - {Name}\n" +
+                $"Store profile - {StoreProfile}\n" +
+                $"Phone number - {PhoneNumber}\n" +
+                $"Email - {Email}\n" +
+                $"Address - {Address}"+
+                $"Area - {Area}";
+        }
+    }
+    
+}
