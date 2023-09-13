6e0e83ec7eed2573daf96ffa7f8b032df233d0bd
Rustam <rustam.niftaliev97@gmail.com>
2023-09-13 15:02:30 +0400

    DZ C#

---
+++ 10.09.2023/10.09.2023/ICalc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._09._2023
+{
+    interface ICalc
+    {
+        int Less(int valueToCompare);
+        int Greater(int valueToCompare);
+    }
+}
---
+++ 10.09.2023/10.09.2023/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using _10._09._2023;
+
+MyArray array = new MyArray();
+
+for (int i = 0; i < 10; i++)
+{
+    array.add(i);
+}
+
+
+Console.WriteLine(array.Less(5));
+Console.WriteLine();
+Console.WriteLine(array.Greater(8));
+
+array.ShowEven();
+array.ShowOdd();
+
+Console.WriteLine(array.CountDistinct());
+Console.WriteLine(array.EqualToValue(5));
+
