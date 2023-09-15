33dae91dafbbc51dc0856576f43b262c8b4ad430
Rustam <rustam.niftaliev97@gmail.com>
2023-09-15 14:30:20 +0400

    DZ C# Queue, LinkedList ...

---
+++ 12.09.2023/12.09.2023/MyLinkedList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._09._2023
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node<T> Previous { get; set; }
+
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+            Previous = null;
+        }
+    }
+
+    public class MyLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public MyLinkedList()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            if (head == null)
+                return false;
+
+            if (head.Data.Equals(data))
+            {
+                head = head.Next;
+                if (head == null)
+                    tail = null;
+                count--;
+                return true;
+            }
+
+            Node<T> current = head;
+            while (current.Next != null)
+            {
+                if (current.Next.Data.Equals(data))
+                {
+                    if (current.Next == tail)
+                        tail = current;
+
+                    current.Next = current.Next.Next;
+                    count--;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void Print()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 12.09.2023/12.09.2023/Program.cs
@@ -0,0 +1,17 @@
+﻿
+#region Task1
+/*int num1 = 1;
+int num2 = 2;
+
+Swap(ref num1, ref num2);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+static void Swap<T>(ref T num1, ref T num2)
+{
+    T num3 = num1;
+    num1 = num2;
+    num2 = num3;
+}*/
+#endregion
+
