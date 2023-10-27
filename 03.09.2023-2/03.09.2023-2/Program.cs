3a840032e1a044d6999036f77c82e1225423de36
Rustam Niftaliev <rustam.niftaliev97@gmail.com>
2023-10-27 14:09:29 +0400

    METRO

--- 03.09.2023-2/03.09.2023-2/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿using _03._09._2023_2;
-Console.WriteLine("Hello");
--- TESTING/TESTING-2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Person
-{
-    public string name { get; set; }
-    public Person() { name = "a"; }
-}
-
-Person d = new();
