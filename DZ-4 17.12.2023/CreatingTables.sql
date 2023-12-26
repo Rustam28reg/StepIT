86d983614cd4426477c59cdddd6d7cf2f2349e7c
Rustam Niftaliev <rustam.niftaliev97@gmail.com>
2023-12-26 11:19:42 +0400

    DZ

---
+++ DZ-4 17.12.2023/CreatingTables.sql
@@ -0,0 +1,48 @@
+create table People(
+[Id] int primary key identity(1, 1),
+[Name] nvarchar(30) NOT NULL,
+[Surname] nvarchar(30) NOT NULL,
+[Age] int NOT NULL check (Age >= 14 and Age < 100)
+);
+
+create table Employee(
+[Id] int primary key identity(1, 1),
+[Salary] smallmoney NOT NULL check ([Salary] >= 300),
+[Experience] int NOT NULL check ([Experience] >= 0)
+);
+
+create table Faculty(
+[Id] int primary key identity(1, 1),
+[Name] nvarchar(30) NOT NULL
+);
+
+create table Students(
+[Id] int primary key identity(1, 1),
+[GPA] int check ([GPA] >= 0 and GPA <= 12),
+[PersonId] int foreign key references People(Id)
+);
+
+create table Teachers(
+[Id] int primary key identity(1, 1),
+[PersonId] int foreign key references People(Id),
+[EmployeeId] int foreign key references Employee(Id)
+);
+
+create table Groups(
+[Id] int primary key identity(1, 1),
+[Name] nvarchar(30) NOT NULL,
+[Course] int NOT NULL check ([Course] >= 1 and [Course] <= 6),
+[FacultyId] int foreign key references Faculty(Id)
+);
+
+create table GroupData(
+[Id] int primary key identity(1, 1),
+[StudentId] int foreign key references Students(Id),
+[GroupId] int foreign key references Groups(Id)
+);
+
+create table StudyPlan(
+[Id] int primary key identity(1, 1),
+[TeacherId] int foreign key references Teachers(Id),
+[GroupId] int foreign key references Groups(Id)
+);
