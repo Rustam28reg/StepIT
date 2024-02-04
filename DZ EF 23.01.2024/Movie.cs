673ce5229eb2f42fcd6c780d8bf06e932b956dab
Rustam Niftaliev <rustam.niftaliev97@gmail.com>
2024-02-04 10:53:38 +0400

    Update

--- DZ EF 23.01.2024/Movie.cs
+++ DZ EF 23.01.2024/Movie.cs
@@ -1,7 +1,7 @@
 ﻿namespace ImdbMovieSearch;
     public class Movie
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Title { get; set; }
         public string Year { get; set; }
         public string MovieType { get; set; }
